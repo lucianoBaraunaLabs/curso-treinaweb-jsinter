6a945cd70bffc805d12af05d458dd658722c3463
= <baraunaluciano@gmail.com>
2017-02-06 06:49:12 -0200

    Continuando desafio

--- desafio_final/codigo_desafio.js
+++ desafio_final/codigo_desafio.js
@@ -9,16 +9,18 @@ $(function(){
     };
     
     // Injecoes de dependencias
-    // funcoes que se autoinvocao injetando o obj e a ordem de inicializacao nao pode ser alterada
-    // 1 config, 2 view, 3 controller. Pois uma depende da outra para trabalhar
+    // funcoes que se autoinvocao injetando o obj, ordem de inicializacao nao pode ser alterada
+    // 1 config, 2 view, 3 controller. As funcoes dependem uma da outra para trabalhar
     (function initConfig(){ // controla as configuracoes
         this.$canvas = $('#snakeCanvas');
         this.gameWidth = this.$canvas.width();
         this.gameHeight = this.$canvas.height();
-        this.cellWidth = 10;
+        this.cellWidth = 15;
+        this.scoreTextStyle = '15px Verdana',
         this.color = {
             background: '#ffffff',
             boardBorder: '#2c3e50',
+            score: '#895434',
             snake: {
                 fill: '#e67e22',
                 border: '#e357ee'
@@ -35,14 +37,23 @@ $(function(){
         var $canvas = config.$canvas[0],
             context = $canvas.getContext('2d');
         
-        // Snake
+        // Celula snake ou comida
         var paintCell = function(x, y, color){
             var cellWidth = config.cellWidth;
-
             context.fillStyle = color.fill;
             context.fillRect(x*cellWidth, y*cellWidth, cellWidth, cellWidth);
             context.strokeStyle = color.border;
             context.strokeRect(x*cellWidth, y*cellWidth, cellWidth, cellWidth);
+        }
+
+        // Snake
+        var paintSnake = function(snake){
+            var snakeColor = config.color.snake;
+            for(indice in snake){
+                var snakeCell = snake[indice];
+                paintCell(snakeCell.x, snakeCell.y, snakeColor);
+            }
+
         }
         
         // Area quadrado
@@ -57,12 +68,27 @@ $(function(){
             context.strokeStyle = boardBorder;
             context.strokeRect(0,0, gameWidth, gameHeight);
         }
+
+        // Placar
+        var paintScore = function(score) {
+            var scoreColor = config.color.score,
+                scoreTextStyle = config.scoreTextStyle;
+            
+            context.font = scoreTextStyle;
+            context.fillStyle = scoreColor;
+            context.fillText(score, 5, 20);
+            
+        }
+
+
         paintGameBoard();
-        paintCell(10, 10, config.color.snake);
+        paintScore(300);
+        
 
 
     }).call(SNAKE.view, SNAKE.config); // SNAKE.config é passado aqui com argumento
                                        // para nao termos um nested muito grande de nomes
+                                       // e para termos o acesso ao obj
     
     (function initController(){
 
